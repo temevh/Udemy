a03f4b19700fe80eff1e8973b6bd3195a5cf376c
temevh <temehama@gmail.com>
2023-03-09 10:05:52 +0200

    working dice game

--- Dicee/index.js
+++ Dicee/index.js
@@ -4,3 +4,11 @@ console.log(randonNumber1);
 
 document.querySelector(".img1").setAttribute("src", "images/dice" + randonNumber1 +".png");
 document.querySelector(".img2").setAttribute("src", "images/dice" + randonNumber2 +".png");
+
+if (randonNumber1 > randonNumber2){
+    document.querySelector("h1").textContent = "Player 1 Won!"
+} else if (randonNumber1 < randonNumber2){
+    document.querySelector("h1").textContent = "Player 2 Won!"
+} else if (randonNumber1 === randonNumber2){
+    document.querySelector("h1").textContent = "Draw!"
+}
