914512bb318f569e38e33f6eb9c373ecba3cfeec
temevh <temehama@gmail.com>
2023-03-10 10:27:59 +0200

    added sound and animation

--- JQuery/game.js
+++ JQuery/game.js
@@ -1,3 +1,14 @@
+const buttonColours = ["red","blue", "green", "yellow"];
+const gamePattern = [];
+
+
 function nextSequence () {
-    
+    var randomNumber = Math.floor((Math.random() * 4));
+    var randomChosenColour = buttonColours[randomNumber];
+    console.log(randomChosenColour);
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
